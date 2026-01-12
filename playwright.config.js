2c4d2e348f375cbb39711d75e179c3140fe366be
Elena Kacher <okacher@proficom.de>
2026-01-12 10:22:04 +0100

    edit config file

--- playwright.config.js
+++ playwright.config.js
@@ -3,8 +3,9 @@ const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   use: {
-    headless: false, // Открывает браузер
     viewport: { width: 1280, height: 720 },
     actionTimeout: 5000,
+    trace: 'on',
+    screenshot: 'on'
   },
 });
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,13 +1,14 @@
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../pages/LoginPage');
 
-test('Успешный вход на saucedemo.com', async ({ page }) => {
+test('Open saucedemo.com', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   await loginPage.goto();
   await loginPage.login('standard_user', 'secret_sauce');
 
-  // Проверяем, что мы попали на страницу с товарами
+  // check the presence of an element inventory_list on the page after login
   await expect(page).toHaveURL(/inventory/);
   await expect(page.locator('.inventory_list')).toBeVisible();
+  await test.pause(); 
 });
