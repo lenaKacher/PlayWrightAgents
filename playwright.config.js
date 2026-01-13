b49a832cb1d0e6a516fdf37fd69350fbd7a4a36e
Elena Kacher <okacher@proficom.de>
2026-01-13 11:23:33 +0100

    remove headed mode and testDir

--- playwright.config.js
+++ playwright.config.js
@@ -31,21 +31,19 @@ module.exports = defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-    headless: false,
+      baseURL: 'https://www.saucedemo.com',
+      trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      testDir: './tests',
       use: { ...devices['Desktop Chrome'] },
     },
 
     {
       name: 'webkit',
-      testDir: './tests/ui',
       use: { ...devices['Desktop Safari'] },
     }
   ],
