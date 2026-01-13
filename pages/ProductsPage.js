0d5aaeadeb890fa6e6b7866d81ad66a7395b3bfb
Elena Kacher <okacher@proficom.de>
2026-01-13 12:04:30 +0100

    create constuctor to locators

--- pages/ProductsPage.js
+++ pages/ProductsPage.js
@@ -4,20 +4,21 @@ class ProductsPage {
    */
   constructor(page) {
     this.page = page;
-
+    this.sortContainer = page.locator('.product_sort_container');
+    this.priceElements = page.locator('.inventory_item_price');
+    this.cartBadge = page.locator('.shopping_cart_badge');
   }
 
   async addToTheBasket(productName) {
-      await this.page.locator('[data-test="add-to-cart-sauce-labs-' + productName + '"]').click();
+    await this.page.locator(`[data-test="add-to-cart-sauce-labs-${productName}"]`).click();
   }
 
   async selectSort(typeOfSort) {
-    await this.page.locator('.product_sort_container').selectOption(typeOfSort);
+    await this.sortContainer.selectOption(typeOfSort);
   }
 
   async getAllProductPrices() {
-    const priceElements = this.page.locator('.inventory_item_price');
-    const count = await priceElements.count();
+    const count = await this.priceElements.count();
 
     if (count === 0) {
       throw new Error('No price elements found on the page');
