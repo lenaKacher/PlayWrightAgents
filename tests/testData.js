08cf90e09df340184888e9b22b273f8fbea8e518
Elena Kacher <okacher@proficom.de>
2026-01-13 11:25:46 +0100

    create file with constants

---
+++ tests/testData.js
@@ -0,0 +1,14 @@
+module.exports = {
+  VALID_USER: {
+    username: 'standard_user',
+    password: 'secret_sauce'
+  },
+  PRODUCTS: {
+    BACKPACK: 'backpack',
+    BIKE_LIGHT: 'bike-light'
+  },
+  EXPECTED_CART_COUNTS: {
+    ONE_ITEM: '1',
+    TWO_ITEMS: '2'
+  }
+};
