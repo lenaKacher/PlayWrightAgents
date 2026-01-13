ae9fe3a4a209b817d2ed3726ee48c9fb6f44e864
Elena Kacher <okacher@proficom.de>
2026-01-13 11:27:19 +0100

    use constants and add methods

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,14 +1,15 @@
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../pages/LoginPage');
+const { VALID_USER, PRODUCTS, EXPECTED_CART_COUNTS } = require('./testData');
 
 test('Open saucedemo.com', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   await loginPage.goto();
-  await loginPage.login('standard_user', 'secret_sauce');
+  await loginPage.login(VALID_USER.username, VALID_USER.password);
 
   // check the presence of an element inventory_list on the page after login
   await expect(page).toHaveURL(/inventory/);
   await expect(page.locator('.inventory_list')).toBeVisible();
-  await page.pause();
+  await expect(page.locator('.product_sort_container')).toBeVisible();
 });
--- tests/product.spec.js
+++ tests/product.spec.js
@@ -1,11 +1,32 @@
 const { test, expect } = require('@playwright/test');
-const { LoginPage } = require('../pages/ProductPage');
+const { ProductsPage } = require('../pages/ProductsPage');
+const { LoginPage } = require('../pages/LoginPage');
+const { VALID_USER, PRODUCTS, EXPECTED_CART_COUNTS } = require('./testData');
 
-test('add item to the basket', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  const productPage = new ProductPage(page);
+test.describe('Product Page Tests', () => {
+  test.beforeEach(async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login(VALID_USER.username, VALID_USER.password);
+  });
 
-  await loginPage.goto();
-  await loginPage.login('standard_user', 'secret_sauce');
+  test('add items to the basket', async ({ page }) => {
+    const productPage = new ProductsPage(page);
+    await productPage.addToTheBasket(PRODUCTS.BACKPACK);
+    await expect(page.locator('.shopping_cart_badge')).toBeVisible();
+    await expect(page.locator('.shopping_cart_badge')).toHaveText(EXPECTED_CART_COUNTS.ONE_ITEM);
 
+    await productPage.addToTheBasket(PRODUCTS.BIKE_LIGHT);
+    await expect(page.locator('.shopping_cart_badge')).toHaveText(EXPECTED_CART_COUNTS.TWO_ITEMS);
+  });
+
+  test('sort products by price: low to high', async ({ page }) => {
+    const productPage = new ProductsPage(page);
+    await productPage.selectSort('Price (low to high)');
+
+    const prices = await productPage.getAllProductPrices();
+    const sortedPrices = [...prices].sort((a, b) => a - b);
+
+    expect(prices).toEqual(sortedPrices);
+  });
 });
